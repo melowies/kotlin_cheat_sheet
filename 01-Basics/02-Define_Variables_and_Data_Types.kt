e65030851df31f08dbb4d2f8f1f1bb914070f902
Selin Üzeyiroğlu <112078937+melowies@users.noreply.github.com>
2025-01-19 15:08:00 +0300

    Create 02-Define_Variables_and_Data_Types.kt

---
+++ 01-Basics/02-Define_Variables_and_Data_Types.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    //DEFINE A VARIABLE 
+    
+    val x = 0 // val keyword - Use when you expect the variable value will not change.
+    var y = 20 // var keyword - Use when you expect the variable value can change.
+    
+    
+    //DATA TYPES
+    
+    val booleanVar: Boolean = true // Boolean can only be true or false
+    val byteVar: Byte = 100 // Byte range: -128 to 127
+    val shortVar: Short = 30000 // Short range: -32,768 to 32,767
+    val intVar: Int = 1000000 // Int range: -2,147,483,648 to 2,147,483,647
+    val longVar: Long = 9000000000L // Long range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+    val floatVar: Float = 3.14F // Float range: 1.4E-45 to 3.4028235E38
+    val doubleVar: Double = 1.7976931348623157E308 // Double range: 4.9E-324 to 1.7976931348623157E308
+    val charVar: Char = 'x' // Char represents a single character, e.g., 'x', 'y'
+    val stringVar: String = "Hey there!" // String can hold a sequence of characters
+
+}
